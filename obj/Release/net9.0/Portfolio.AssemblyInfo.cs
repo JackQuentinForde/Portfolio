c866805da344e3852065cfdd1f25b9f893dc206c
JackQuentinForde <jackforde96@gmail.com>
2025-12-07 15:22:53 +0000

    more responsive redesign

--- obj/Release/net9.0/Portfolio.AssemblyInfo.cs
+++ obj/Release/net9.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33decd20c218ee3205a7146e7ee1fb1bb2593d20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08aed5048958f15721ad75f0953e15d792b6907f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
